192db0c664b01828174cc15987c12f23a82720c5
StaneCobalt <scsladek91@gmail.com>
2018-10-28 21:18:29 -0500

    Add teleport boundaries
    Also removed unused scripts

--- Assets/Scripts/HandControllerInput.cs
+++ Assets/Scripts/HandControllerInput.cs
@@ -15,6 +15,11 @@ public class HandControllerInput : MonoBehaviour {
 	public static float yNudgeAmount = 1f; // specific to teleportAimerObject height
 	private static readonly Vector3 yNudgeVector = new Vector3(0f, yNudgeAmount, 0f);
 
+	// Boundary Checks
+	public float minXBoundary = -12.0f, maxXBoundary = 12.0f;
+	public float minYBoundary = 0.0f, maxYBoundary = 2.0f;
+	public float minZBoundary = -12.0f, maxZBoundary = 12.0f;
+
 	// Use this for initialization
 	void Start () {
 		trackedObject = GetComponent<SteamVR_TrackedObject>();
@@ -43,14 +48,9 @@ public class HandControllerInput : MonoBehaviour {
 			if (Physics.Raycast(transform.position, transform.forward, out hit, 15, laserMask)) {
 				teleportLocation = hit.point;
 				laser.SetPosition(1, teleportLocation);
-				teleportAimerObject.transform.position = new Vector3(teleportLocation.x, 
-					teleportLocation.y + yNudgeAmount, 
-					teleportLocation.z);
-			} else {
-				//teleportLocation = new Vector3(transform.forward.x * 15 + transform.position.x,
-				//	transform.forward.y * 15 + transform.position.y,
-				//	transform.forward.z * 15 + transform.position.z);
 
+				teleportAimerObject.transform.position = BoundaryCheck(teleportLocation, true);
+			} else {
 				teleportLocation = transform.position + 15 * transform.forward;
 				RaycastHit groundRay;
 				if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 17, laserMask)) {
@@ -62,13 +62,32 @@ public class HandControllerInput : MonoBehaviour {
 			//laser.SetPosition(1, transform.forward * 15 + transform.position);
 			//teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
 			setLaserEnd(teleportLocation);
-			teleportAimerObject.transform.position = teleportLocation + yNudgeVector;
+			//teleportAimerObject.transform.position = teleportLocation + yNudgeVector;
+			teleportAimerObject.transform.position = BoundaryCheck(teleportLocation, true);
 		}
 		// trigger released
 		if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
 			laser.gameObject.SetActive(false);
-			teleportAimerObject.SetActive(false);       
-			player.transform.position = teleportLocation;   
+			teleportAimerObject.SetActive(false);
+			//player.transform.position = teleportLocation;
+			player.transform.position = BoundaryCheck(teleportLocation, false);
 		}
+	}
+
+	Vector3 BoundaryCheck(Vector3 pos, bool isAimer)
+	{
+		pos.x = (pos.x > maxXBoundary) ? maxXBoundary : pos.x;
+		pos.x = (pos.x < minXBoundary) ? minXBoundary : pos.x;
+
+		pos.y = (pos.y > maxYBoundary) ? maxYBoundary : pos.y;
+		pos.y = (pos.y < minYBoundary) ? minYBoundary : pos.y;
+
+		pos.z = (pos.z > maxZBoundary) ? maxZBoundary : pos.z;
+		pos.z = (pos.z < minZBoundary) ? minZBoundary : pos.z;
+
+		if (isAimer)
+			return new Vector3(pos.x, pos.y + yNudgeAmount, pos.z);
+		else
+			return new Vector3(pos.x, pos.y, pos.z);
 	}
 }
